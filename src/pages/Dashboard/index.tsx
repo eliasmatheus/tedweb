7e9e900d1cd18789a7cd5b009998302a9f916c5e
kayque <kayque.avelar@somosagility.com.br>
2021-06-14 17:36:39 -0300

    peakers

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,113 @@
 import React from 'react';
 
-import { Title } from './styles';
+import logoHeader from '../../assets/logo-header.png';
+import contrapondoParadigmas from '../../assets/contrapondo-paradigmas.png';
+import firstSpeaker from '../../assets/firstSpeaker.png';
+import secondSpeaker from '../../assets/secondSpeaker.png';
+import thirdSpeaker from '../../assets/thirdSpeaker.png';
+
+import {
+  Header,
+  HeaderBody,
+  HeaderTop,
+  SpeakersSection,
+  DescriptionSpaker,
+} from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <Header>
+        <HeaderTop>
+          <img src={logoHeader} alt="TEDx Belo Horizonte" />
+        </HeaderTop>
+        <HeaderBody>
+          <img src={contrapondoParadigmas} alt="Contrapondo Paradigmas" />
+        </HeaderBody>
+      </Header>
+      <SpeakersSection>
+        <h2>PALESTRANTES</h2>
+        <div>
+          <div>
+            <div>
+              <img
+                src="https://media-exp3.licdn.com/dms/image/C4D03AQGfJp428KvEVQ/profile-displayphoto-shrink_800_800/0/1576464863674?e=1629331200&v=beta&t=yWSW47vOUt-rhwjy7jJJ9k1FfiVMsmUCY0nFiPg8c5E"
+                alt="FirstSpeaker"
+              />
+            </div>
+            <DescriptionSpaker>
+              <h6>João Pedro Santos de Moura</h6>
+              <div>Software Engineer | .NET | Node.js</div>
+            </DescriptionSpaker>
+          </div>
+
+          <div>
+            <div>
+              <img
+                src="https://media-exp3.licdn.com/dms/image/C4D03AQHGjUUPtRG_7w/profile-displayphoto-shrink_800_800/0/1613746381551?e=1629331200&v=beta&t=F8GtwAKb9nEpXPSikvtZLoCjXO1YffmaC-K-PmEbiV0"
+                alt="FirstSpeaker"
+              />
+            </div>
+            <DescriptionSpaker>
+              <h6>Fernanda Sousa</h6>
+              <div>Analista de Produto | UX/UI na B2W Digital</div>
+            </DescriptionSpaker>
+          </div>
+
+          <div>
+            <div>
+              <img
+                src="https://media-exp3.licdn.com/dms/image/C4E03AQFxu-jFEGoIEQ/profile-displayphoto-shrink_800_800/0/1612732331486?e=1629331200&v=beta&t=OGQVJgvBSSKZwqRhrnrnaAbXmb-9fkgyy8cfJxd8tag"
+                alt="FirstSpeaker"
+              />
+            </div>
+            <DescriptionSpaker>
+              <h6>João Vitor Innecco Arêas</h6>
+              <div>Lead Organizer at TEDxBeloHorizonte</div>
+            </DescriptionSpaker>
+          </div>
+        </div>
+        {/* <div>
+          <div>
+            <div>
+              <img
+                src="https://media-exp3.licdn.com/dms/image/C4D03AQGfJp428KvEVQ/profile-displayphoto-shrink_800_800/0/1576464863674?e=1629331200&v=beta&t=yWSW47vOUt-rhwjy7jJJ9k1FfiVMsmUCY0nFiPg8c5E"
+                alt="FirstSpeaker"
+              />
+            </div>
+            <div>
+              <h3>Luiza Helena Trajano Inácio Rodrigues</h3>
+              <p>CEO Magazine Luiza</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img
+                src="https://media-exp3.licdn.com/dms/image/C4D03AQHGjUUPtRG_7w/profile-displayphoto-shrink_800_800/0/1613746381551?e=1629331200&v=beta&t=F8GtwAKb9nEpXPSikvtZLoCjXO1YffmaC-K-PmEbiV0"
+                alt="FirstSpeaker"
+              />
+            </div>
+            <div>
+              <h3>Luiza Helena Trajano Inácio Rodrigues</h3>
+              <p>CEO Magazine Luiza</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img
+                src="https://media-exp3.licdn.com/dms/image/C4E03AQFxu-jFEGoIEQ/profile-displayphoto-shrink_800_800/0/1612732331486?e=1629331200&v=beta&t=OGQVJgvBSSKZwqRhrnrnaAbXmb-9fkgyy8cfJxd8tag"
+                alt="FirstSpeaker"
+              />
+            </div>
+            <div>
+              <h3>Luiza Helena Trajano Inácio Rodrigues</h3>
+              <p>CEO Magazine Luiza</p>
+            </div>
+          </div>
+        </div> */}
+      </SpeakersSection>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,71 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: #3a3a3a;
+export const Header = styled.div`
+  height: 481px;
+`;
+
+export const HeaderTop = styled.div`
+  background: white;
+  height: 113px;
+  padding-top: 31px;
+  padding-left: 28px;
+`;
+
+export const HeaderBody = styled.div`
+  padding-top: 64px;
+`;
+
+export const SpeakersSection = styled.div`
+  background: white;
+  height: 855px;
+
+  display: flex;
+  /* justify-content: center; */
+  flex-direction: column;
+  align-items: center;
+  padding-top: 102px;
+  h2 {
+    font-size: 36px;
+    font-weight: 500;
+    margin-bottom: 50px;
+  }
+  div {
+    display: flex;
+    width: 100%;
+    div {
+      flex-direction: column;
+      div {
+        width: 100%;
+        img {
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
+export const DescriptionSpaker = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  flex-direction: column;
+  /* padding-top: 38px;
+  padding-bottom: 38px;
+  padding-left: 41px;
+  padding-right: 41px; */
+  height: 140px;
+  font-size: 24px;
+  font-family: Helvetica;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 28px;
+  h6 {
+    font-size: 24px;
+    font-family: Helvetica;
+    font-style: normal;
+    font-weight: normal;
+    line-height: 28px;
+  }
+  div {
+    color: #ff1100;
+  }
 `;
